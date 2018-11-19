57d63e72288c3059c487c863d168f0a7e066538b
juniornelson123 <junior123nelson@gmail.com>
2018-11-19 17:58:09 -0300

    screens for examples

---
+++ lib/detail.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Detail extends StatefulWidget {
+  @override
+  _DetailState createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
---
+++ lib/image.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'detail.dart';
+
+class ImageScreen extends StatefulWidget {
+  @override
+  _ImageScreenState createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+            'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
+          ),
+        ),
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return Detail();
+          }));
+        },
+      ),
+    );
+  }
+}
